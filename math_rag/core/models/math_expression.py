122680e2a45d52d725ebaba0b95730c2a08e9516
Luka958Pixion <luka.panic@pixion.co>
2025-03-05 15:18:14 +0100

    added backoff

--- math_rag/core/models/math_expression.py
+++ math_rag/core/models/math_expression.py
@@ -6,6 +6,6 @@ from pydantic import BaseModel, Field
 class MathExpression(BaseModel):
     id: UUID = Field(default_factory=uuid4)
     latex: str
-    katex: str
+    katex: str | None
     position: int
     is_inline: bool
--- math_rag/infrastructure/inference/llm.py
+++ math_rag/infrastructure/inference/llm.py
@@ -1,13 +1,18 @@
-from openai import AsyncOpenAI
+from backoff import expo, on_exception
+from openai import AsyncOpenAI, RateLimitError
 
 from math_rag.application.base.inference import BaseLLM, TBaseLLMResponseModel
 from math_rag.application.models import LLMParams
+
+
+retry = on_exception(expo, RateLimitError, max_time=60, max_tries=6)
 
 
 class LLM(BaseLLM):
     def __init__(self, client: AsyncOpenAI):
         self.client = client
 
+    @retry
     async def generate_text(self, prompt: str, params: LLMParams) -> str:
         completion = await self.client.chat.completions.create(
             model=params.model,
@@ -20,6 +25,7 @@ class LLM(BaseLLM):
 
         return completion.choices[0].message.content
 
+    @retry
     async def generate_json(
         self,
         prompt: str,
