53feb2d4c3b51b5e5b0f5889c186334b074a204e
Luka958Pixion <luka.panic@pixion.co>
2025-03-06 20:54:36 +0100

    updated BaseLLM

--- math_rag/application/base/inference/base_llm.py
+++ math_rag/application/base/inference/base_llm.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Type
 
-from math_rag.application.models import LLMRequest, LLMResponse
+from math_rag.application.models import (
+    LLMRequest,
+    LLMRequestBatch,
+    LLMResponse,
+    LLMResponseBatch,
+)
 from math_rag.application.types import LLMResponseType
 
 
@@ -9,4 +15,24 @@ class BaseLLM(ABC):
     async def generate(
         self, request: LLMRequest[LLMResponseType]
     ) -> list[LLMResponse[LLMResponseType]]:
+        pass
+
+    @abstractmethod
+    async def batch_generate(
+        self,
+        request_batch: LLMRequestBatch[LLMResponseType],
+        response_type: Type[LLMResponseType],
+    ) -> LLMResponseBatch[LLMResponseType] | None:
+        pass
+
+    @abstractmethod
+    async def batch_generate_init(
+        self, request_batch: LLMRequestBatch[LLMResponseType]
+    ) -> str:
+        pass
+
+    @abstractmethod
+    async def batch_generate_result(
+        self, batch_id: str, response_type: Type[LLMResponseType]
+    ) -> LLMResponseBatch[LLMResponseType] | None:
         pass
