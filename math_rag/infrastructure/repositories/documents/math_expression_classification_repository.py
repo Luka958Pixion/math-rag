180b3d2c0b646613df5e30c4f10ed1e82928aff2
Luka958Pixion <luka.panic@pixion.co>
2025-03-08 19:20:23 +0100

    updated mongo repos

--- math_rag/infrastructure/repositories/documents/math_expression_classification_repository.py
+++ math_rag/infrastructure/repositories/documents/math_expression_classification_repository.py
@@ -5,32 +5,16 @@ from math_rag.infrastructure.models.documents import (
     MathExpressionClassificationDocument,
 )
 
+from .common_repository import CommonRepository
 
-class MathExpressionClassificationRepository:
+
+class MathExpressionClassificationRepository(
+    CommonRepository[MathExpressionClassificationDocument, MathExpressionClassification]
+):
     def __init__(self, client: AsyncMongoClient, deployment: str):
-        self.client = client
-        self.db = self.client[deployment]
-        self.collection_name = MathExpressionClassification.__name__.lower()
-        self.collection = self.db[self.collection_name]
-
-    async def insert_math_expression_classifications(
-        self, items: list[MathExpressionClassification]
-    ):
-        docs = [
-            MathExpressionClassificationDocument.from_internal(item) for item in items
-        ]
-        bson_docs = [doc.model_dump() for doc in docs]
-
-        await self.collection.insert_many(bson_docs)
-
-    async def get_math_expression_classifications(
-        self, limit: int
-    ) -> list[MathExpressionClassification]:
-        cursor = self.collection.find().limit(limit)
-        bson_docs = await cursor.to_list(length=limit)
-        docs = [
-            MathExpressionClassificationDocument(**bson_doc) for bson_doc in bson_docs
-        ]
-        items = [MathExpressionClassificationDocument.to_internal(doc) for doc in docs]
-
-        return items
+        super().__init__(
+            client=client,
+            deployment=deployment,
+            document_cls=MathExpressionClassificationDocument,
+            internal_cls=MathExpressionClassification,
+        )
