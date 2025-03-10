dae645f9b75533365d5630f1216cbe74e272291b
Luka958Pixion <luka.panic@pixion.co>
2025-03-10 18:24:05 +0100

    removed redundancy from OpenAIConcurrentLLM

--- math_rag/infrastructure/inference/openai_concurrent_llm.py
+++ math_rag/infrastructure/inference/openai_concurrent_llm.py
@@ -26,56 +26,15 @@ from math_rag.infrastructure.constants.inference import (
     SECONDS_TO_PAUSE_AFTER_RATE_LIMIT_ERROR,
     SECONDS_TO_SLEEP_EACH_LOOP,
 )
-from math_rag.infrastructure.mappings.inference import LLMResponseListMapping
+from math_rag.infrastructure.mappings.inference import (
+    LLMRequestMapping,
+    LLMResponseListMapping,
+)
 
 
 class OpenAIConcurrentLLM(BaseConcurrentLLM):
     def __init__(self, client: AsyncOpenAI):
         self.client = client
-
-    async def _generate_text(
-        self,
-        request: LLMRequest[LLMResponseType],
-    ) -> LLMResponseList[LLMResponseType]:
-        completion = await self.client.chat.completions.create(
-            model=request.params.model,
-            messages=[
-                {'role': message.role, 'content': message.content}
-                for message in request.conversation.messages
-            ],
-            response_format={'type': 'text'},
-            temperature=request.params.temperature,
-            logprobs=request.params.top_logprobs is not None,
-            top_logprobs=request.params.top_logprobs,
-            reasoning_effort=request.params.reasoning_effort,
-            max_completion_tokens=request.params.max_completion_tokens,
-        )
-        response_list = LLMResponseListMapping[LLMResponseType].to_source(completion)
-
-        return response_list
-
-    async def _generate_json(
-        self,
-        request: LLMRequest[LLMResponseType],
-    ) -> LLMResponseList[LLMResponseType]:
-        parsed_completion = await self.client.beta.chat.completions.parse(
-            model=request.params.model,
-            messages=[
-                {'role': message.role, 'content': message.content}
-                for message in request.conversation.messages
-            ],
-            response_format=request.params.response_type,
-            temperature=request.params.temperature,
-            logprobs=request.params.top_logprobs is not None,
-            top_logprobs=request.params.top_logprobs,
-            reasoning_effort=request.params.reasoning_effort,
-            max_completion_tokens=request.params.max_completion_tokens,
-        )
-        response_list = LLMResponseListMapping[LLMResponseType].to_source(
-            parsed_completion
-        )
-
-        return response_list
 
     async def _generate(
         self,
@@ -84,14 +43,19 @@ class OpenAIConcurrentLLM(BaseConcurrentLLM):
         status_tracker: LLMStatusTracker,
         response_lists: list[LLMResponseList[LLMResponseType]],
     ):
+        request = request_tracker.request
         error = None
 
         try:
-            request = request_tracker.request
-            response_list = (
-                await self._generate_text(request)
+            request_dict = LLMRequestMapping[LLMResponseType].to_target(request)
+            completion_callback = (
+                self.client.chat.completions.create
                 if request.params.response_type is LLMTextResponse
-                else await self._generate_json(request)
+                else self.client.beta.chat.completions.parse
+            )
+            completion = await completion_callback(**request_dict)
+            response_list = LLMResponseListMapping[LLMResponseType].to_source(
+                completion
             )
 
         except RateLimitError as e:
