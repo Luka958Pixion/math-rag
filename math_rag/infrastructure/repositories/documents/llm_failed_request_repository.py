25b8967b8c2fa687bc65bbf013549f1bba6d14eb
Luka958Pixion <luka.panic@pixion.co>
2025-03-11 17:13:14 +0100

    updated repo

--- math_rag/infrastructure/repositories/documents/llm_failed_request_repository.py
+++ math_rag/infrastructure/repositories/documents/llm_failed_request_repository.py
@@ -1,5 +1,8 @@
 from pymongo import AsyncMongoClient
 
+from math_rag.application.base.repositories.documents import (
+    BaseLLMFailedRequestRepository,
+)
 from math_rag.core.models import MathExpression
 from math_rag.infrastructure.mappings.documents import LLMFailedRequestMapping
 from math_rag.infrastructure.models.documents import LLMFailedRequestDocument
@@ -8,9 +11,10 @@ from .document_repository import DocumentRepository
 
 
 class LLMFailedRequestRepository(
+    BaseLLMFailedRequestRepository,
     DocumentRepository[
         MathExpression, LLMFailedRequestDocument, LLMFailedRequestMapping
-    ]
+    ],
 ):
     def __init__(self, client: AsyncMongoClient, deployment: str):
         super().__init__(client, deployment)
