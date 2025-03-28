1bbb218a466b56b3fd6f255573ec099c4cebce57
Luka958Pixion <luka.panic@pixion.co>
2025-03-28 20:15:39 +0000

    moved SCPClient to separate dir

--- math_rag/infrastructure/clients/__init__.py
+++ math_rag/infrastructure/clients/__init__.py
@@ -1,6 +1,7 @@
 from .apptainer_client import ApptainerClient
 from .arxiv_client import ArxivClient
 from .katex_client import KatexClient
+from .scp_client import SCPClient
 
 
-__all__ = ['ApptainerClient', 'ArxivClient', 'KatexClient']
+__all__ = ['ApptainerClient', 'ArxivClient', 'KatexClient', 'SCPClient']
---
+++ math_rag/infrastructure/clients/scp_client.py
@@ -0,0 +1,41 @@
+from contextlib import AsyncExitStack
+from pathlib import Path
+from typing import AsyncGenerator
+
+from aiofiles import open
+from asyncssh import connect, read_private_key
+
+
+class SCPClient:
+    def __init__(self, host: str, user: str, passphrase: str):
+        self.host = host
+        self.user = user
+        self.private_key = read_private_key('/.ssh/id_ed25519', passphrase=passphrase)
+
+    async def secure_copy(
+        self,
+        source: Path | AsyncGenerator[bytes, None],
+        target: Path,
+    ):
+        if isinstance(source, Path):
+            source = self.stream_file(source)
+
+        async with AsyncExitStack() as stack:
+            conn = await stack.enter_async_context(
+                connect(self.host, username=self.user, client_keys=[self.private_key])
+            )
+            sftp = await stack.enter_async_context(conn.start_sftp_client())
+            file = await stack.enter_async_context(sftp.open(str(target), 'wb'))
+
+            async for chunk in source:
+                await file.write(chunk)
+
+    async def stream_file(self, path: Path) -> AsyncGenerator[bytes, None]:
+        async with open(path, 'rb') as file:
+            while True:
+                chunk = await file.read(8192)
+
+                if not chunk:
+                    break
+
+                yield chunk
