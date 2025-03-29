ab94f5834494f00a959634236da25b38b7dfb691
Luka958Pixion <luka.panic@pixion.co>
2025-03-29 11:31:59 +0000

    updated PBSProVariableList

--- math_rag/infrastructure/models/pbs/pbs_pro_variable_list.py
+++ math_rag/infrastructure/models/pbs/pbs_pro_variable_list.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class PBSProVariableList(BaseModel):
@@ -11,22 +11,30 @@ class PBSProVariableList(BaseModel):
         https://help.altair.com/2024.1.0/PBS%20Professional/PBSReferenceGuide2024.1.pdf#page=245
     """
 
-    home: Path | None = Field(alias='PBS_O_HOME')
-    path: Path | None = Field(alias='PBS_O_PATH')
-    mail: Path | None = Field(alias='PBS_O_MAIL')
-    shell: Path | None = Field(alias='PBS_O_SHELL')
-    workdir: Path | None = Field(alias='PBS_O_WORKDIR')
-    host: str | None = Field(alias='PBS_O_HOST')
-    lang: str | None = Field(alias='PBS_O_LANG')
-    logname: str | None = Field(alias='PBS_O_LOGNAME')
-    system: str | None = Field(alias='PBS_O_SYSTEM')
-    queue: str | None = Field(alias='PBS_O_QUEUE')
-    tz: str | None = Field(alias='PBS_O_TZ')
+    home: Path | None = Field(default=None, alias='PBS_O_HOME')
+    paths: list[Path] | None = Field(default=None, alias='PBS_O_PATH')
+    mail: str | None = Field(default=None, alias='PBS_O_MAIL')
+    shell: str | None = Field(default=None, alias='PBS_O_SHELL')
+    workdir: Path | None = Field(default=None, alias='PBS_O_WORKDIR')
+    host: str | None = Field(default=None, alias='PBS_O_HOST')
+    lang: str | None = Field(default=None, alias='PBS_O_LANG')
+    logname: str | None = Field(default=None, alias='PBS_O_LOGNAME')
+    system: str | None = Field(default=None, alias='PBS_O_SYSTEM')
+    queue: str | None = Field(default=None, alias='PBS_O_QUEUE')
+    tz: str | None = Field(default=None, alias='PBS_O_TZ')
 
-    environment: str | None = Field(alias='PBS_ENVIRONMENT')
-    jobdir: Path | None = Field(alias='PBS_JOBDIR')
-    jobid: str | None = Field(alias='PBS_JOBID')
-    jobname: str | None = Field(alias='PBS_JOBNAME')
-    nodefile: Path | None = Field(alias='PBS_NODEFILE')
-    execution_queue: str | None = Field(alias='PBS_QUEUE')
-    tmpdir: Path | None = Field(alias='PBS_TMPDIR')
+    environment: str | None = Field(default=None, alias='PBS_ENVIRONMENT')
+    jobdir: Path | None = Field(default=None, alias='PBS_JOBDIR')
+    jobid: str | None = Field(default=None, alias='PBS_JOBID')
+    jobname: str | None = Field(default=None, alias='PBS_JOBNAME')
+    nodefile: Path | None = Field(default=None, alias='PBS_NODEFILE')
+    execution_queue: str | None = Field(default=None, alias='PBS_QUEUE')
+    tmpdir: Path | None = Field(default=None, alias='PBS_TMPDIR')
+
+    @field_validator('paths', mode='before')
+    @classmethod
+    def split_path(cls, value: str | None) -> list[Path] | None:
+        if not value:
+            return None
+
+        return [Path(path) for path in value.split(':') if path]
