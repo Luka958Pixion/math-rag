8597d7754d7af4b5dadb37ce26b1ad57422c1086
Luka958Pixion <luka.panic@pixion.co>
2025-04-08 09:12:15 +0000

    init_resources bugfix

--- math_rag/infrastructure/inference/huggingface/huggingface_batch_llm.py
+++ math_rag/infrastructure/inference/huggingface/huggingface_batch_llm.py
@@ -75,7 +75,6 @@ class HuggingFaceBatchLLM(PartialBatchLLM):
 
         for local_path in local_paths:
             remote_path = self.remote_project_root / local_path.name
-            has_file_path = await self.hpc_client.has_file_path(remote_path)
 
             if await self.hpc_client.has_file_path(remote_path):
                 if not await self.hpc_client.has_file_changed(local_path, remote_path):
