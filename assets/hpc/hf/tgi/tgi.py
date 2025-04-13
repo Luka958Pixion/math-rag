2091af2d64a7dc263a14555638d80489bf3a35f7
Luka958Pixion <luka.panic@pixion.co>
2025-04-13 11:19:02 +0000

    migrated form httpx to http.client

--- assets/hpc/hf/tgi/tgi.py
+++ assets/hpc/hf/tgi/tgi.py
@@ -4,19 +4,17 @@ import subprocess
 
 from datetime import timedelta
 from enum import Enum
+from http.client import HTTPConnection
 from logging import INFO, basicConfig, getLogger
 from pathlib import Path
 from queue import PriorityQueue
 from threading import Thread
 from time import sleep
+from urllib.parse import urlparse
 from uuid import UUID
 
-from decouple import config
-from httpx import Client
-from pydantic import BaseModel
 
-
-PBS_JOB_ID = config('PBS_JOBID')
+PBS_JOB_ID = os.environ['PBS_JOBID']
 
 WORKDIR = Path('.')
 
@@ -31,11 +29,6 @@ WALLTIME_THRESHOLD = timedelta(minutes=10)
 
 basicConfig(level=INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = getLogger(__name__)
-
-
-class TGIMetadata(BaseModel):
-    batch_id: UUID
-    model_hub_id: str
 
 
 class TGIBatchJobStatus(str, Enum):
@@ -69,11 +62,11 @@ class TGIBatchJobStatusTracker:
             del self._statuses[task_id]
 
 
-class TGIStatus(BaseModel):
+class TGIStatus:
     pass
 
 
-class Metadata(BaseModel):
+class BatchJobMetadata:
     batch_request_id: UUID
     model_hub_id: str
     timestamp: int
@@ -86,7 +79,7 @@ class HuggingFaceCLI:
         env['http_proxy'] = HTTP_PROXY
         env['https_proxy'] = HTTPS_PROXY
 
-        bind = f'{WORKDIR}/data:/data'
+        bind = f'{WORKDIR}/mount:/mount'
         cmd = f'apptainer run --nv --bind {bind} --env MODEL_HUB_ID={model_hub_id} hf_cli.sif'
         subprocess.run(cmd, check=True, capture_output=False, shell=True, env=env)
 
@@ -100,22 +93,36 @@ class TGIServerInstance:
         cmd = f'apptainer instance start --nv --bind {bind} server.sif tgi_server_instance'
         subprocess.run(cmd, check=True, capture_output=False, shell=True)
 
+        POLL_INTERVAL = 5
+        parsed_url = urlparse(TGI_BASE_URL)
+
+        if not parsed_url.port:
+            raise ValueError('TGI_BASE_URL does not include a port')
+
+        client = HTTPConnection(parsed_url.hostname, parsed_url.port, timeout=3)
         logger.info('Waiting for TGI server to become healthy...')
-        client = Client()
-        poll_interval = 5
 
         while True:
-            response = client.get(TGI_BASE_URL + '/health', timeout=3)
+            try:
+                client.request('GET', '/health')
+                response = client.getresponse()
 
-            if response.status_code == 200:
-                logger.info('TGI server is healthy.')
-                break
+                if response.status == 200:
+                    logger.info('TGI server is healthy.')
+                    break
 
-            logger.info(
-                f'Health check returned status {response.status_code}. '
-                f'Retrying in {poll_interval}s...'
-            )
-            sleep(poll_interval)
+                else:
+                    logger.info(
+                        f'Health check returned status {response.status}. '
+                        f'Retrying in {POLL_INTERVAL}s...'
+                    )
+
+            except Exception as e:
+                logger.info(
+                    f'Health check failed: {e}. Retrying in {POLL_INTERVAL}s...'
+                )
+
+            sleep(POLL_INTERVAL)
 
     @staticmethod
     def stop():
@@ -150,7 +157,7 @@ def watch_walltime():
         pass
 
 
-def read_metadata_file(metadata_queue: PriorityQueue[Metadata]):
+def read_metadata_file(metadata_queue: PriorityQueue[BatchJobMetadata]):
     while True:
         # read metadata files
         metadata_paths = WORKDIR.glob('metadata_*.json')
@@ -158,33 +165,45 @@ def read_metadata_file(metadata_queue: PriorityQueue[Metadata]):
         for metadata_path in metadata_paths:
             with metadata_path.open('r') as file:
                 data = json.load(file)
-                metadata = Metadata(**data)
+                metadata = BatchJobMetadata(**data)
                 metadata_queue.put((metadata.timestamp, metadata))
 
             # delete metadata files after reading
             metadata_path.unlink()
 
+        sleep(60)
 
-def process_batch_request(metadata_queue: PriorityQueue[Metadata]):
+
+def process_batch_request(metadata_queue: PriorityQueue[BatchJobMetadata]):
     metadata = metadata_queue.get()
     mount_path = WORKDIR / 'mount' / metadata.model_hub_id
-    mount_path.mkdir(parents=True, exist_ok=True)
 
-    HuggingFaceCLI.download_model(metadata.model_hub_id)
-    TGIServerInstance.start(mount_path)
+    if not mount_path.exists():
+        mount_path.mkdir(parents=True)
+        HuggingFaceCLI.download_model(metadata.model_hub_id)
+
+    # TODO: check if instance with the same model is running from previous batch
+    previous_metadata: BatchJobMetadata | None = ...
+
+    if not previous_metadata:
+        TGIServerInstance.start(mount_path)
+
+    elif previous_metadata.model_hub_id != metadata.model_hub_id:
+        TGIServerInstance.stop()
+        TGIServerInstance.start(mount_path)
+
     TGIClient.run()
-    TGIServerInstance.stop()
 
 
 def main():
     # TODO automatically shut down job after 15 minutes of inactivity
-    poll_interval = 60
 
+    # TODO
     status = TGIStatus()
     status_json = status.model_dump_json()
     TGI_STATUS_PATH.write_text(status_json)
 
-    metadata_queue: PriorityQueue[Metadata] = PriorityQueue()
+    metadata_queue: PriorityQueue[BatchJobMetadata] = PriorityQueue()
 
     metadata_reader_thread = Thread(
         target=read_metadata_file, args=(metadata_queue), name='MetadataReaderThread'
@@ -205,8 +224,6 @@ def main():
     metadata_reader_thread.join()
     walltime_watcher_thread.join()
     batch_request_processor_thread.join()
-
-    sleep(poll_interval)
 
 
 if __name__ == '__main__':
