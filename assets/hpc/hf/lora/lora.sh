b0c28cb0d002d21ec066d357c8d7694b25ba9582
Luka958Pixion <luka.panic@pixion.co>
2025-04-20 18:32:10 +0000

    lora wip

---
+++ assets/hpc/hf/lora/lora.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -P gpu
+#PBS -o output.log
+#PBS -e error.log
+#PBS -q gpu
+
+cd "${PBS_O_WORKDIR:-""}"
+module load utils/python/3.12.2
+
+export PYTHONUNBUFFERED=1
+
+python3 lora.py
