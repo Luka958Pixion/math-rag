41b6324501caa47f1f47c17854b6f47e45139f41
Luka958Pixion <luka.panic@pixion.co>
2025-04-22 11:20:21 +0000

    added model support for TokenCounterUtil

--- math_rag/infrastructure/utils/token_counter_util.py
+++ math_rag/infrastructure/utils/token_counter_util.py
@@ -1,35 +1,47 @@
 from typing import overload
 
-from tiktoken import get_encoding
+from tiktoken import encoding_for_model, get_encoding
 
 from math_rag.application.models.inference import EMRequest, LLMRequest
 from math_rag.application.types.inference import LLMResponseType
-from math_rag.infrastructure.constants.inference.openai import (
-    ENCODING_NAME,
-    MAX_COMPLETION_TOKENS,
-)
 
 
 class TokenCounterUtil:
     @overload
     @staticmethod
-    def count(request: EMRequest, encoding_name: str | None = None) -> int:
+    def count(
+        request: EMRequest,
+        *,
+        encoding_name: str | None = None,
+        model_name: str | None = None,
+    ) -> int:
         pass
 
     @overload
     @staticmethod
     def count(
-        request: LLMRequest[LLMResponseType], encoding_name: str | None = None
+        request: LLMRequest[LLMResponseType],
+        *,
+        encoding_name: str | None = None,
+        model_name: str | None = None,
     ) -> int:
         pass
 
     @staticmethod
     def count(
         request: EMRequest | LLMRequest[LLMResponseType],
+        *,
         encoding_name: str | None = None,
+        model_name: str | None = None,
     ) -> int:
-        encoding_name = encoding_name or ENCODING_NAME
-        encoding = get_encoding(encoding_name)
+        if encoding_name:
+            encoding = get_encoding(encoding_name)
+
+        elif model_name:
+            encoding = encoding_for_model(model_name)
+
+        else:
+            raise ValueError('Missing encoding_name or model_name')
 
         if isinstance(request, EMRequest):
             return len(encoding.encode(request.text))
@@ -38,9 +50,7 @@ class TokenCounterUtil:
             len(encoding.encode(message.content))
             for message in request.conversation.messages
         )
-        max_completion_tokens = (
-            request.params.max_completion_tokens or MAX_COMPLETION_TOKENS
-        )
+        max_completion_tokens = request.params.max_completion_tokens or 0
         completion_tokens = request.params.n * max_completion_tokens
         total_tokens = prompt_tokens + completion_tokens
 
