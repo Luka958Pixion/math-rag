055855ada11ef4d3aaaa76b97067f6a651de8b2d
Luka958Pixion <luka.panic@pixion.co>
2025-05-07 21:26:58 +0000

    updated PBSProJobListenerManager

--- math_rag/infrastructure/managers/pbs_pro_job_listener_manager.py
+++ math_rag/infrastructure/managers/pbs_pro_job_listener_manager.py
@@ -9,28 +9,28 @@ class PBSProJobListenerManager:
         self._lock = Lock()
         self._job_id_to_entry: dict[UUID, PBSProJobListenerEntry] = {}
 
-    def add_or_update_job_id(self, job_id: UUID, listener_classes: list[str]):
+    def add_job_id(self, job_id: UUID, listener_classes: set[str]):
         with self._lock:
             entry = self._job_id_to_entry.get(job_id)
 
             if not entry:
                 entry = PBSProJobListenerEntry(
-                    job_id=job_id, listener_classes=set(listener_classes)
+                    job_id=job_id, listener_classes=listener_classes
                 )
                 self._job_id_to_entry[job_id] = entry
 
             else:
                 entry.listener_classes.update(listener_classes)
 
-    def get_job_ids(self, listener: str) -> list[UUID]:
+    def get_job_ids(self, listener_class: str) -> list[UUID]:
         with self._lock:
             job_ids: list[UUID] = []
             job_ids_to_remove: list[UUID] = []
 
             for job_id, entry in self._job_id_to_entry.items():
-                if listener in entry.listener_classes:
+                if listener_class in entry.listener_classes:
                     job_ids.append(job_id)
-                    entry.listener_classes.remove(listener)
+                    entry.listener_classes.remove(listener_class)
 
                     if not entry.listener_classes:
                         job_ids_to_remove.append(job_id)
