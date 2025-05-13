09197ab0bf8772baef0b7293e81edfbc734c942b
Luka958Pixion <luka.panic@pixion.co>
2025-05-13 11:10:10 +0000

    added batching to MathArticleLoaderService

--- math_rag/application/services/math_article_loader_service.py
+++ math_rag/application/services/math_article_loader_service.py
@@ -1,9 +1,16 @@
+from asyncio import gather
+
+from arxiv import Result
+
 from math_rag.application.base.clients import BaseArxivClient
 from math_rag.application.base.repositories.objects import BaseMathArticleRepository
 from math_rag.application.base.services import BaseMathArticleLoaderService
 from math_rag.application.enums.arxiv import BaseArxivCategory
 from math_rag.core.models import MathArticle
 from math_rag.shared.utils import GzipExtractorUtil
+
+
+BATCH_SIZE = 5
 
 
 class MathArticleLoaderService(BaseMathArticleLoaderService):
@@ -20,42 +27,48 @@ class MathArticleLoaderService(BaseMathArticleLoaderService):
             raise ValueError()
 
         sublimit = int(limit / len(BaseArxivCategory))
-        results = [
-            result
-            for subcategory in category
-            for result in self.arxiv_client.search(subcategory, sublimit)
-        ]
-        file_name_to_bytes: dict[str, bytes] = {}
+        category_list = list(category)
 
-        for result in results:
-            arxiv_id = result.entry_id.split('/')[-1]
-            src = await self.arxiv_client.get_src(arxiv_id)
-            # NOTE: we dont need pdfs at the moment
-            # pdf = await arxiv_searcher_service.get_pdf(arxiv_id)
+        for i in range(0, len(category_list), BATCH_SIZE):
+            batch = category_list[i : i + BATCH_SIZE]
+            results = [
+                result
+                for subcategory in batch
+                for result in self.arxiv_client.search(subcategory, sublimit)
+            ]
+            process_tasks = [self._process_result(result) for result in results]
+            processed_files = await gather(*process_tasks)
+            math_articles = [
+                MathArticle(name=name, bytes=bytes)
+                for d in processed_files
+                if d
+                for name, bytes in d.items()
+            ]
+            self.math_article_repository.insert_many(math_articles)
 
-            if src is None:
-                continue
+    async def _process_result(self, result: Result) -> dict[str, bytes] | None:
+        arxiv_id = result.entry_id.split('/')[-1]
+        src = await self.arxiv_client.get_src(arxiv_id)
 
-            src_name, src_bytes = src
+        if src is None:
+            return None
 
-            if not src_name or src_name.endswith('.pdf'):
-                continue
+        src_name, src_bytes = src
 
-            if src_name.endswith('.tar.gz'):
-                extracted_file_to_bytes = GzipExtractorUtil.extract_tar_gz(src_bytes)
-                file_name_to_bytes.update(
-                    {f'{arxiv_id}/{k}': v for k, v in extracted_file_to_bytes.items()}
-                )
+        if not src_name or src_name.endswith('.pdf'):
+            return None
 
-            elif src_name.endswith('.gz'):
-                extracted_bytes = GzipExtractorUtil.extract_gz(src_bytes)
-                file_name_to_bytes[f'{arxiv_id}.tex'] = extracted_bytes
+        if src_name.endswith('.tar.gz'):
+            extracted_file_to_bytes = GzipExtractorUtil.extract_tar_gz(src_bytes)
 
-            else:
-                raise ValueError(f'Unexpected file extension {src_name}')
+            return {
+                f'{arxiv_id}/{key}': value
+                for key, value in extracted_file_to_bytes.items()
+            }
 
-        math_articles = [
-            MathArticle(name=name, bytes=bytes)
-            for name, bytes in file_name_to_bytes.items()
-        ]
-        self.math_article_repository.insert_many(math_articles)
+        if src_name.endswith('.gz'):
+            extracted_bytes = GzipExtractorUtil.extract_gz(src_bytes)
+
+            return {f'{arxiv_id}.tex': extracted_bytes}
+
+        raise ValueError(f'Unexpected file extension {src_name}')
