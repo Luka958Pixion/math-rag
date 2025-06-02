52b0de57b552f215fb8a12ac09390d2fb760c6ae
Luka958Pixion <luka.panic@pixion.co>
2025-06-02 10:52:06 +0000

    updated FineTuneSettingsLoaderService

--- math_rag/application/services/fine_tune_settings_loader_service.py
+++ math_rag/application/services/fine_tune_settings_loader_service.py
@@ -4,13 +4,11 @@ from math_rag.application.models.fine_tune.settings import FineTuneSettings
 from math_rag.shared.utils import YamlLoaderUtil
 
 
+BASE_PATH = Path(__file__).parents[3] / 'settings' / 'fine_tune'
+
+
 class FineTuneSettingsLoaderService:
     def load(self, provider_name: str, model_name: str) -> FineTuneSettings:
-        path = (
-            Path(__file__).parents[3]
-            / 'settings'
-            / 'fine_tune'
-            / provider_name
-            / f'{model_name}.yaml'
-        )
+        path = BASE_PATH / provider_name / f'{model_name}.yaml'
+
         return YamlLoaderUtil.load(path, model=FineTuneSettings)
