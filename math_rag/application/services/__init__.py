ae165c2271382dc53b0c2c466a63eab108f0fbd3
Luka958Pixion <luka.panic@pixion.co>
2025-06-02 14:51:19 +0000

    removed fine_tune_settings_loader_service

--- math_rag/application/services/__init__.py
+++ math_rag/application/services/__init__.py
@@ -4,9 +4,7 @@ from .llm_settings_loader_service import LLMSettingsLoaderService
 from .math_article_loader_service import MathArticleDatasetLoaderService
 from .math_article_parser_service import MathArticleParserService
 from .math_expression_dataset_builder_service import MathExpressionDatasetBuilderService
-from .math_expression_dataset_publisher_service import (
-    MathExpressionDatasetPublisherService,
-)
+from .math_expression_dataset_publisher_service import MathExpressionDatasetPublisherService
 from .math_expression_label_loader_service import MathExpressionLabelLoaderService
 from .math_expression_loader_service import MathExpressionLoaderService
 from .math_expression_sample_loader_service import MathExpressionSampleLoaderService
