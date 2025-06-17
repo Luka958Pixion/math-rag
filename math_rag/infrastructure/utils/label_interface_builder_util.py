092b28978e14a6670db830eecd551680deba0b58
Luka958Pixion <luka.panic@pixion.co>
2025-06-17 15:28:30 +0000

    implemented LabelInterfaceBuilderUtil

--- math_rag/infrastructure/utils/label_interface_builder_util.py
+++ math_rag/infrastructure/utils/label_interface_builder_util.py
@@ -18,10 +18,11 @@ class LabelInterfaceBuilderUtil:
 
         for field_name, tag_type in field_name_to_tag_type.items():
             if type(tag_type) is Choices:
+                choices = [Choice(value=label_name) for label_name in label_names]
                 tags[field_name] = Choices(
                     name=field_name,
                     choice='single',
-                    choices=[Choice(value=label_name) for label_name in label_names],
+                    choices=choices,
                 )
 
             elif type(tag_type) is HyperText:
