37eec88c4f34ddaab166adb0fc90cddcfb94582b
Luka958Pixion <luka.panic@pixion.co>
2025-06-18 12:57:23 +0000

    bugfix

--- math_rag/application/base/services/base_label_config_builder_service.py
+++ math_rag/application/base/services/base_label_config_builder_service.py
@@ -1,11 +1,7 @@
 from abc import ABC, abstractmethod
 
-from math_rag.infrastructure.types.labels.tags import TagType
-
 
 class BaseLabelConfigBuilderService(ABC):
     @abstractmethod
-    def build(
-        self, field_name_to_tag_type: dict[str, type[TagType]], label_names: list[str]
-    ) -> str:
+    def build(self, field_name_to_tag_name: dict[str, str], label_names: list[str]) -> str:
         pass
